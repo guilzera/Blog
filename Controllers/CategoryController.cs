d57595d9e1b0e2a7daf4f1450015e733ce063bdb
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-10-06 21:44:02 -0300

    Banco de dados, modelos e primeiros controllers criados

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(BlogDataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
